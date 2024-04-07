0ccb0a45cf7cea39a3c6c0994907aec7e23386e2
Jiyong <75185178+Ataraxia1001@users.noreply.github.com>
2024-04-08 00:06:37 +0900

    almost solved

---
+++ Valid_Palindrome_II.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def validPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = list(s)
+        cnt = 0
+        for i in range(len(s)/2):
+            if s[i] == s[-i-1]:
+                continue
+            else:
+                if cnt >= 1:
+                    print('cnt > 1')
+                    return False
+            
+                elif s[i] == s[-i-2] and cnt_right < 1:  
+                    cnt += 1
+                    s.pop(-i-1)
+                    print('pop -i-1')
+                    continue
+                elif s[i+1] == s[-i-1] and cnt_left < 1:
+                    print(cnt)
+                    cnt += 1
+                    s.pop(i)
+                    print('pop i')
+                    continue
+                else:
+                    return False
+        return True
